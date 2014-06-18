1e79cb01b36e84bdedb9174ff0cf82ef7f3ed6e0
Elzbieta Siwy <ela@lshift.net>
2014-06-18 17:51:22 +0100

    Removed close function from the configReader

--- scripts/configReader.py
+++ scripts/configReader.py
@@ -2,12 +2,11 @@
 class ConfigReader:
 	def __init__(self, fileName):
 		self.fp = open(fileName, 'r')
+		self.emails = self.getEmails()
+		self.fp.close()
 
 	def getEmails(self):
 		emails = []
 		for line in self.fp:
 			emails.append(line.strip())
 		return emails
-
-	def close(self):
-		self.fp.close()
