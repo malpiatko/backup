e0c756f79600385168c80362fd9bd76e4887afe2
Elzbieta Siwy <ela@lshift.net>
2014-08-07 16:36:41 +0100

    In the middle of code review. Changing naming conventions.

--- scripts/md5dir.py
+++ scripts/md5dir.py
@@ -32,7 +32,7 @@ output_help = """Writes the changes to the specified file instead of the
   standard output."""
 compare_help = """Compares the two md5sum files given as arguments."""
 twodir_help = """Compares the two directories given as arguments. Creates the
-  hashfiles for the directories only if their location is explicitely
+  hashfiles for the directories only if their location is explicitly
   specified."""
 suppress_help = """When set the program does not output the changes. Suitable
   for initialising a directory."""
@@ -46,7 +46,6 @@ dirs_help = """A list of directories to perform the analysis for. By default
   changed by adding an additional argument specifying the path in which the
   file should be found. Therefore each item of the list can bo of the form
   dirpath[:filepath]."""
-
 description_msg = """By default it checks for differences between the current
   state of each directory specified as arguments and its previous version saved
   in the 'md5sum' files, subsequently updating the files. The 'md5sum' file
@@ -57,24 +56,24 @@ description_msg = """By default it checks for differences between the current
 class Md5dir(object):
     """ Md5dir """
 
-    hashfilename = "md5sum"  # Default name for checksum file.
-    ignores = []         # By default don't ignore any files.
-    suppresschanges = False
+    hashfilename = "md5sum"     # Default name for checksum file.
+    ignores = []                # By default don't ignore any files.
+    dirs = []
 
     # Regular expression for lines in GNU md5sum file
     md5line = re.compile(r"^([0-9a-f]{32}) [\ \*](.*)$")
 
     def log(self, msg):
-        """ Writes given message to the relevant output."""
+        """Writes given message to the relevant output."""
         if not self.suppresschanges:
-            output.write(msg + "\n")
+            self.output.write(msg + "\n")
 
-    def toignore(self, filename):
-        if filter(lambda patt: fnmatch.fnmatch(filename, patt), self.ignores):
-            return True
-        return False
+    def ignore(self, filename):
+        """Ignore when at least one matches found."""
+        return any([fnmatch.fnmatch(filename, i) for i in self.ignores])
 
     def getDictionary(self, filename):
+        """Converts the md5sum file into a dictionary of filename -> md5sum."""
         # If file doesn't exists we return an empty dictionary.
         if not op.isfile(filename):
             return
@@ -88,25 +87,24 @@ class Md5dir(object):
                        "md5": match.group(1)}
 
     def master_list(self, start):
+        """Return a list of files relative to start directory."""
         # Collect all files under start (follow directory symbolic links).
         for root, dirs, files in os.walk(start, followlinks=True):
-            if self.toignore(root):
+            if self.ignore(root):
                 continue
             for fname in files:
                 fname = op.join(root, fname)
-                if self.toignore(fname):
+                if self.ignore(fname):
                     continue
-                # Take care of symbolic links pointing to a file.
                 try:
                     if op.islink(fname):
-                            # We are using this command to check whether the link
-                            # is not broken.
-                            os.stat(fname)
-                            fname = os.readlink(fname)
-                            # os.readlink() may or maynot return absolute path. If
-
-                            if not op.isabs(fname):
-                                fname = op.join(root, fname)
+                        # We are using this command to check whether the link
+                        # is not broken.
+                        os.stat(fname)
+                        fname = os.readlink(fname)
+                        # os.readlink() may or maynot return absolute path. If
+                        if not op.isabs(fname):
+                            fname = op.join(root, fname)
                     yield op.relpath(fname, start)
                 except OSError, e:
                     if e.errno == errno.ENOENT:
@@ -117,23 +115,23 @@ class Md5dir(object):
     def makesums(self, root, hashfile):
         """Creates an md5sum file for the given directory
         in the location specified."""
-        progress("Creating md5sum file.")
+        self.progress("Creating md5sum file.")
         with open(hashfile, "w") as fp:
             for index, fname in enumerate(self.master_list(root)):
-                if verbose and index % verbose == 0:
-                    progress("Computing md5, %d files analyzed." % index)
+                if self.verbose and index % self.verbose == 0:
+                    self.progress("Computing md5, %d files analyzed." % index)
                 newhash = self.calcsum(op.join(root, fname), self.mp3mode)
                 if newhash != -1:
                     fp.write("%s  %s\n" % (newhash, fname))
-        progress("Sorting the md5sum file.")
+        self.progress("Sorting the md5sum file.")
         subprocess.call(["sort", "-k", "2", "-o", hashfile, hashfile])
-        progress("Finished sorting the md5sum file.")
+        self.progress("Finished sorting the md5sum file.")
 
     def calcsum(self, filepath):
         """Return md5 checksum for a file. Uses the tag-skipping algorithm
         for .mp3 files if in mp3mode."""
         if self.mp3mode and filepath.endswith(".mp3"):
-            return calculateUID(filepath)
+            return self.calculateUID(filepath)
 
         h = md5.new()
         try:
@@ -157,7 +155,7 @@ class Md5dir(object):
         def neckst(item):
             return next(item, -1)
 
-        progress("Comparing the md5sum files.")
+        self.progress("Comparing the md5sum files.")
         confirmed = 0
         changed = 0
         added = 0
@@ -172,10 +170,10 @@ class Md5dir(object):
             # Match. Both proceed to next element.
             pathold = dictold["file"]
             pathnew = dictnew["file"]
-            if self.toignore(pathold):
+            if self.ignore(pathold):
                 dictold = neckst(old)
                 continue
-            if self.toignore(pathnew):
+            if self.ignore(pathnew):
                 dictnew = neckst(new)
                 continue
             if pathold == pathnew:
@@ -212,60 +210,105 @@ class Md5dir(object):
         self.log("STATUS: confirmed %d added %d deleted %d changed %d" % (
             confirmed, added, deleted, changed))
 
+    # TODO: Maybe with class easier way to do the counts.
+    def progress(self, message):
+        """If the verbose flag is set output progress message."""
+        if self.verbose:
+            print "PROGRESS: %s" % message
 
-def calculateUID(filepath):
-    """Calculate MD5 for an MP3 excluding ID3v1 and ID3v2 tags if
-    present. See www.id3.org for tag format specifications."""
-    f = open(filepath, "rb")
-    # Detect ID3v1 tag if present
-    finish = os.stat(filepath).st_size
-    f.seek(-128, 2)
-    if f.read(3) == "TAG":
-        finish -= 128
-    # ID3 at the start marks ID3v2 tag (0-2)
-    f.seek(0)
-    start = f.tell()
-    if f.read(3) == "ID3":
-        # Bytes w major/minor version (3-4)
-        # Flags byte (5)
-        flags = struct.unpack("B", f.read(1))[0]
-        # Flat bit 4 means footer is present (10 bytes)
-        footer = flags & (1 << 4)
-        # Size of tag body synchsafe integer (6-9)
-        bs = struct.unpack("BBBB", f.read(4))
-        bodysize = (bs[0] << 21) + (bs[1] << 14) + (bs[2] << 7) + bs[3]
-        # Seek to end of ID3v2 tag
-        f.seek(bodysize, 1)
-        if footer:
-            f.seek(10, 1)
-        # Start of rest of the file
+    def addDirectories(self, dirs):
+        for item in dirs:
+            pair = item.split(":")
+            directory = {"path": pair[0]}
+            directory["file"] = pair[1] if len(pair) == 2 else None
+            self.dirs.append(directory)
+
+    def analyzeDirs(self):
+        # Treat each argument separately
+            for arg in self.dirs:
+                start = arg["directory"]
+                if not op.isdir(start):
+                    print "Argument %s is not a directory" % start
+                    continue
+                if arg["file"]:
+                    hashfile = op.abspath(arg["file"])
+                else:
+                    hashfile = op.join(op.abspath(start), self.hashfilename)
+
+                # Copy the content of md5sum into a temporary file.
+                file1 = tempfile.NamedTemporaryFile(delete=False)
+                if op.isfile(hashfile):
+                    with open(hashfile, "r") as fp:
+                        shutil.copyfileobj(fp, file1)
+                file1.close()
+                # Update the hashfile.
+                self.makesums(start, hashfile)
+                # Compare the files.
+                self.compare(file1.name, hashfile)
+                os.unlink(file1.name)
+
+    def twoDirComparison(self):
+        arg1 = self.dirs[0]
+        arg2 = self.dirs[1]
+        if not op.isdir(arg1["path"]) or not op.isdir(arg2["path"]):
+            return False
+        file1 = arg1["file"] or \
+            tempfile.NamedTemporaryFile(delete=False).name
+        file2 = arg2["file"] or \
+            tempfile.NamedTemporaryFile(delete=False).name
+        self.makesums(arg1["path"], file1)
+        self.makesums(arg2["path"], file2)
+        self.compare(file1, file2)
+        return True
+
+    def setignores(self, filepath):
+        if not filepath:
+            self.ignores = []
+            return
+        with open(filepath, 'r') as f:
+            doc = yaml.load(f)
+        self.ignores = doc["ignore"]
+
+    @staticmethod
+    def calculateUID(filepath):
+        """Calculate MD5 for an MP3 excluding ID3v1 and ID3v2 tags if
+        present. See www.id3.org for tag format specifications.
+        Function entirely taken from http://snipplr.com/view/4023/ and
+        not tested."""
+        f = open(filepath, "rb")
+        # Detect ID3v1 tag if present
+        finish = os.stat(filepath).st_size
+        f.seek(-128, 2)
+        if f.read(3) == "TAG":
+            finish -= 128
+        # ID3 at the start marks ID3v2 tag (0-2)
+        f.seek(0)
         start = f.tell()
-    # Calculate MD5 using stuff between tags
-    f.seek(start)
-    h = md5.new()
-    h.update(f.read(finish - start))
-    f.close()
-    return h.hexdigest()
-
-
-def getignores(filepath):
-    with open(filepath, 'r') as f:
-        doc = yaml.load(f)
-    return doc["ignore"]
-
-
-def progress(message):
-    """If the verbose flag is set output progress message."""
-    if verbose:
-        print "PROGRESS: %s" % message
+        if f.read(3) == "ID3":
+            # Bytes w major/minor version (3-4)
+            # Flags byte (5)
+            flags = struct.unpack("B", f.read(1))[0]
+            # Flat bit 4 means footer is present (10 bytes)
+            footer = flags & (1 << 4)
+            # Size of tag body synchsafe integer (6-9)
+            bs = struct.unpack("BBBB", f.read(4))
+            bodysize = (bs[0] << 21) + (bs[1] << 14) + (bs[2] << 7) + bs[3]
+            # Seek to end of ID3v2 tag
+            f.seek(bodysize, 1)
+            if footer:
+                f.seek(10, 1)
+            # Start of rest of the file
+            start = f.tell()
+        # Calculate MD5 using stuff between tags
+        f.seek(start)
+        h = md5.new()
+        h.update(f.read(finish - start))
+        f.close()
+        return h.hexdigest()
 
 
 if __name__ == "__main__":
-    global output
-    global mp3mode
-    global suppresschanges
-    global verbose
-    dirs = []
+    md5dir = Md5dir()
 
     # Parse command-line options
     parser = argparse.ArgumentParser(description=description_msg)
@@ -284,15 +327,13 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # Store global variables.
-    output = args.output
-    mp3mode = args.mp3
-    suppresschanges = args.suppresschanges
-    verbose = args.verbose
+    md5dir.output = args.output
+    md5dir.mp3mode = args.mp3
+    md5dir.suppresschanges = args.suppresschanges
+    md5dir.verbose = args.verbose
 
     # Saves files and directories to ignore.
-    if args.ignore:
-        ignores = getignores(args.ignore)
+    md5dir.ignores = md5dir.setignores(args.ignore)
 
     # Compare two md5sum files.
     if args.comparefiles:
@@ -301,54 +342,22 @@ if __name__ == "__main__":
         if not op.isfile(file1) or not op.isfile(file2):
             print "Exiting because the arguments are not files."
             sys.exit(0)
-        compare(file1, file2)
+        md5dir.compare(file1, file2)
+
     # All other options use the dirs command line argument.
     else:
-        for item in args.dirs:
-            pair = item.split(":")
-            directory = {"path": pair[0]}
-            directory["file"] = pair[1] if len(pair) == 2 else None
-            dirs.append(directory)
+        md5dir.addDirectories(args.dirs)
 
         # Compare two directories
         if args.twodir:
-            if len(dirs) != 2:
+            if len(md5dir.dirs) != 2:
                 print "Two arguments expected."
                 sys.exit(0)
-            arg1 = dirs[0]
-            arg2 = dirs[1]
-            if not op.isdir(arg1["path"]) or not op.isdir(arg2["path"]):
+
+            if not md5dir.twoDirComparison():
                 print "Exiting because arguments are not directory pathnames."
                 sys.exit()
-            file1 = arg1["file"] or \
-                tempfile.NamedTemporaryFile(delete=False).name
-            file2 = arg2["file"] or \
-                tempfile.NamedTemporaryFile(delete=False).name
-            makesums(arg1["path"], file1)
-            makesums(arg2["path"], file2)
-            compare(file1, file2)
 
         # Analyze the given directories.
         else:
-            # Treat each argument separately
-            for arg in dirs:
-                start = arg["directory"]
-                if not op.isdir(start):
-                    print "Argument %s is not a directory" % start
-                    continue
-                if arg["file"]:
-                    hashfile = op.abspath(arg["file"])
-                else:
-                    hashfile = op.join(op.abspath(start), hashfilename)
-
-                # Copy the content of md5sum into a temporary file.
-                file1 = tempfile.NamedTemporaryFile(delete=False)
-                if op.isfile(hashfile):
-                    with open(hashfile, "r") as fp:
-                        shutil.copyfileobj(fp, file1)
-                file1.close()
-                # Update the hashfile.
-                makesums(start, hashfile)
-                # Compare the files.
-                compare(file1.name, hashfile)
-                os.unlink(file1.name)
+            md5dir.analyzeDirs()
